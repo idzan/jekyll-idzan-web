066151bcf1250788f7df1468ec1aa4913f5040f9
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-04-24 14:48:38 +0200

    test for klaro cookie consent loading with external script
    quick test with loading script in external code with onclick feature #letitworkbaby

--- js/consentChange.js
+++ js/consentChange.js
@@ -1,16 +1,17 @@
 /* function klaroCookieChange() {
     return klaro.show();
 }
-
+ */
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#cookieChange').addEventListener('click', clickHandler);
-    klaroCookieChange();
-}); */
+    let changeCookie = document.querySelector('#cookieChange');
+    changeCookie.onclick = klaro.show();
+    return false;
+});
 
 
 //Just a test
-window.onload = function() {
+/* window.onload = function() {
     var changeCookie = document.querySelector("#cookieChange");
     changeCookie.onclick =  klaro.show();
     return false;
-}
+} */
