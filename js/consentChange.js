2aea9c72eedc9168b1a432cab268e8dda7f54cd7
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-04-24 14:52:38 +0200

    Update consentChange.js
    bit of update for testings otherwise it will work only once and later will not show, so close to finish this crap aaaaaa

--- js/consentChange.js
+++ js/consentChange.js
@@ -1,11 +1,11 @@
-/* function klaroCookieChange() {
-    return klaro.show();
-}
- */
-document.addEventListener('DOMContentLoaded', function () {
-    let changeCookie = document.querySelector('#cookieChange');
-    changeCookie.onclick = klaro.show();
+function klaroCookieChange() {
+    klaro.show();
     return false;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#cookieChange').addEventListener('click', clickHandler);
+    klaroCookieChange();
 });
 
 
