7f490b34a2ad9f893d85d81974f1af567a68dd2c
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-04-27 14:19:09 +0200

    Update consentChange.js
    Removed crap from code

--- js/consentChange.js
+++ js/consentChange.js
@@ -1,20 +1,7 @@
-/* function klaroCookieChange() {
-    klaro.show();
-    return false;
-}
- */
+
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#cookieChange').addEventListener('click', function() {
         klaro.show();
         return false;
     });
-    /* klaroCookieChange(); */
 });
-
-
-//Just a test
-/* window.onload = function() {
-    var changeCookie = document.querySelector("#cookieChange");
-    changeCookie.onclick =  klaro.show();
-    return false;
-} */
